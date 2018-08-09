cc6322b9240a7a036d50ff8b0dd457625fdfeaf0
Alfredo Egranfonte <egranfont@gmail.com>
2018-08-09 13:22:38 -0300

    Atualizado arquivo loader Aula 46

--- loader.js
+++ loader.js
@@ -7,8 +7,9 @@ const bodyParser = require('body-parser' )
 //framework web express
 const express = require('express')
 const server = express()
-//para toda requisicao que chegar dentro do backend será passado por esse midleware urlencoded
+//para toda requisicao que chegar dentro do backend será passado por esse middleware urlencoded
 server.use(bodyParser.urlencoded({extended: true}))
+//outro middleware para fazer o parse do json
 server.use(bodyParser.json())
 
 server.listen(porta, function(){
@@ -16,5 +17,14 @@ server.listen(porta, function(){
 })
 
 server.use(function (req,res,next){
-    res.send('Funcionou!')
+    //const init = Date.now()
+    //console.log(` ---------------------- `)
+    console.log(' meu middleware 1 ')
+    next()
+})
+//middleware que serve para todas as urls da aplicação
+server.use(function (req,res,next){
+    console.log(' meu middleware 2 ')
+    res.send(' Funcionou novamente!!!')
+    
 })
